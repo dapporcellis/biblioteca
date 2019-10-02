ee55051fc09b68bdf625896269f785c8af4736d6
Diego de Abreu Porcellis <dapporcellis@hotmail.com>
2019-10-02 07:59:39 -0300

    conexao decrapted resolvido

--- modelo/conexao.js
+++ modelo/conexao.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
 const endereco = "mongodb://localhost:27017/biblioteca"
 
-mongoose.connect(endereco, { useNewUrlParser: true })
+mongoose.connect(endereco, { useNewUrlParser: true, useUnifiedTopology: true })
 
 module.exports = mongoose
